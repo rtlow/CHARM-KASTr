2289c03c72760d148180d44559df3570a0f13e65
rtlow <37086221+rtlow@users.noreply.github.com>
2018-11-24 05:48:57 -0800

    Updated mask.py
    mask.py now creates a mask based on the medians along rows and columns instead of individual pixel values.

---
+++ py/bias_subtract.py
@@ -0,0 +1,7 @@
+
+import numpy as np
+
+def bias_subtract(image, bias):
+    subtr = image - bias
+
+    return subtr
--- py/floader.py
+++ py/floader.py
@@ -6,12 +6,27 @@ biaspath = "/home/ryan/projects/CHARMKASTR/raw/data-2017-08-12-shane-Adam.Burgas
 
 flatpath = "/home/ryan/projects/CHARMKASTR/raw/data-2017-08-12-shane-Adam.Burgasser/red/test1035/flat/"
 
+sciencepath = "/home/ryan/projects/CHARMKASTR/raw/data-2017-08-12-shane-Adam.Burgasser/red/test1035/science/"
+
+arcpath = "/home/ryan/projects/CHARMKASTR/raw/data-2017-08-12-shane-Adam.Burgasser/red/test1035/arc/"
+
 BIAS = []
 
 FLAT = []
+
+SCIENCE = []
+
+ARC = []
 
 for filename in glob.glob(biaspath + '*.fits'):
     BIAS.append(fits.open(filename))
 
 for filename in glob.glob(flatpath + '*.fits'):
     FLAT.append(fits.open(filename))
+
+for filename in glob.glob(sciencepath + '*.fits'):
+    SCIENCE.append(fits.open(filename))
+
+for filename in glob.glob(arcpath + '*.fits'):
+    ARC.append(fits.open(filename))
+
