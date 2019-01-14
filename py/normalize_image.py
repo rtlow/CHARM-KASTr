Turns out no problems with normalization. To do: Separate File IO from extraction functions (just fix up median()), Implement Wavelength Calibration, Figure out next steps.

py/normalize_image.py
@@ -1,10 +1,18 @@
 import numpy as np
 import copy
 
-def normalize_image(image, norm_flat):
+def normalize_image(image, norm_flat, mask):
     
     norm_image = copy.deepcopy(image)
     
-    norm_image = np.divide(norm_image, norm_flat)
-        
+    nf = copy.deepcopy(norm_flat)
+
+    m = copy.deepcopy(mask)
+
+    for i in range(nf.shape[0]):
+        for j in range(nf.shape[1]):
+            if m[i][j] == 0:
+                norm_image[i][j] = np.divide(norm_image[i][j], nf[i][j])
+
+
     return norm_image

py/variance_image.py
@@ -1,18 +1,19 @@
 import numpy as np
 import copy
 
-#gains are in units [e-/DN]
-gain_slow = 1.9
-
-gain_fast = 0.55
-
-#Shane KAST red read noise are in units [e-]
-read_noise_slow = 3.8
-
-read_noise_fast = 4.3
-
 
 def variance_image( image ):
+    
+    #gains are in units [e-/DN]
+    gain_slow = 1.9
+
+    gain_fast = 0.55
+
+    #Shane KAST red read noise are in units [e-]
+    read_noise_slow = 3.8
+
+    read_noise_fast = 4.3
+    
     var_img = copy.deepcopy(image)
 
     with np.nditer(var_img, op_flags=['readwrite']) as it: